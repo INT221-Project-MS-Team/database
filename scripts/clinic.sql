4c2510fb70b957ce49ea841dbee78e454e4523d9
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-30 23:43:41 +0700

    Update Charset

--- scripts/clinic.sql
+++ scripts/clinic.sql
@@ -15,8 +15,8 @@ DROP TABLE IF EXISTS `clinic`.`event_category` ;
 
 CREATE TABLE IF NOT EXISTS `clinic`.`event_category` (
   `eventCategoryId` INT NOT NULL,
-  `eventCategoryName` VARCHAR(100) NOT NULL,
-  `eventCategoryDescription` VARCHAR(500) NULL,
+  `eventCategoryName` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `eventCategoryDescription` VARCHAR(500) COLLATE utf8mb4_general_ci NULL,
   `eventDuration` INT NOT NULL,
   PRIMARY KEY (`eventCategoryId`),
   UNIQUE INDEX `eventCatagoryName_UNIQUE` (`eventCategoryName` ASC) VISIBLE)
@@ -28,11 +28,11 @@ DROP TABLE IF EXISTS `clinic`.`event` ;
 
 CREATE TABLE IF NOT EXISTS `clinic`.`event` (
   `eventId` INT NOT NULL,
-  `bookingName` VARCHAR(100) NOT NULL,
-  `bookingEmail` VARCHAR(255) NOT NULL,
+  `bookingName` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `bookingEmail` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
   `eventStartTime` DATETIME NOT NULL,
   `eventDuration` INT NOT NULL,
-  `eventNotes` VARCHAR(500) NULL,
+  `eventNotes` VARCHAR(500) COLLATE utf8mb4_general_ci NULL,
   `eventCategoryId` INT NOT NULL,
   PRIMARY KEY (`eventId`),
   INDEX `fk_event_event-catagory_idx` (`eventCategoryId` ASC) VISIBLE,
@@ -48,28 +48,20 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-insert into event_category (eventCategoryId,eventCategoryName,eventCategoryDescription,eventDuration)
-values (1,'Project Management Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย project management clinic ในวิชา INT221 integrated project I ให้นักศึกษาเตรียมเอกสารที่เกี่ยวข้องเพื่อแสดงระหว่างขอคําปรึกษา',30);
-insert into event_category (eventCategoryId,eventCategoryName,eventCategoryDescription,eventDuration)
-values (2,'DevOps/Infra Clinic','Use this event category for DevOps/Infra clinic.',20);
-insert into event_category (eventCategoryId,eventCategoryName,eventCategoryDescription,eventDuration)
-values (3,'Database Clinic','ตารางนัดหมายนี้ใช้สําหรับนัดหมาย database clinic ในวิชา INT221 integrated project I',15);
-insert into event_category (eventCategoryId,eventCategoryName,eventCategoryDescription,eventDuration)
-values (4,'Client-side Clinic','ตารางนัดหมายนี้ใช้สําหรับนัดหมาย client-side clinic ในวิชา INT221 integrated project I',30);
-insert into event_category (eventCategoryId,eventCategoryName,eventDuration)
-values (5,'Server-side Clinic',30);
+INSERT INTO `event_category` (`eventCategoryId`, `eventCategoryName`, `eventCategoryDescription`, `eventDuration`) VALUES
+(1, 'Project Management Clinic', 'ตารางนัดหมายนี้ใช้สำหรับนัดหมาย project management clinic ในวิชา INT221 integrated project I ให้นักศึกษาเตรียมเอกสารที่เกี่ยวข้องเพื่อแสดงระหว่างขอคําปรึกษา', 30),
+(2, 'DevOps/Infra Clinic', 'Use this event category for DevOps/Infra clinic.', 20),
+(3, 'Database Clinic', 'ตารางนัดหมายนี้ใช้สําหรับนัดหมาย database clinic ในวิชา INT221 integrated project I', 15),
+(4, 'Client-side Clinic', 'ตารางนัดหมายนี้ใช้สําหรับนัดหมาย client-side clinic ในวิชา INT221 integrated project I', 30),
+(5, 'Server-side Clinic', NULL, 30);
 
+INSERT INTO `event` (`eventId`, `bookingName`, `bookingEmail`, `eventStartTime`, `eventDuration`, `eventNotes`, `eventCategoryId`) VALUES
+(1, 'Somchai Jaidee(OR-7)', 'somchai.jai@mail.kmutt.ac.th', '2022-05-23 13:30:00', 30, NULL, 2),
+(2, 'SomsriRakdee(SJ-3)', 'somsri.rak@mail.kmutt.ac.th', '2022-05-27 09:30:00', 30, 'ขอปรึกษาปัญหาเพื่อนไม่ช่วยงาน', 1),
+(3, 'สมเกียรติ ขยันเรียน กลุ่ม TT-4', 'somkiat.kay@kmutt.ac.th', '2022-05-23 16:30:00', 15, NULL, 3),
+(4, 'หิวข้าวงับ', 'eiei.za@kmutt.ac.th', '2022-05-29 08:30:00', 15, NULL, 1),
+(5, 'test', 'test.test@kmutt.ac.th', '2022-05-30 01:30:00', 20, NULL, 2);
 
-insert into event (eventId,bookingName,bookingEmail,eventStartTime,eventDuration,eventCategoryId)
-values (1,'Somchai Jaidee(OR-7)','somchai.jai@mail.kmutt.ac.th','2022-05-23 13:30:00',30,2);
-insert into event (eventId,bookingName,bookingEmail,eventStartTime,eventDuration,eventNotes,eventCategoryId)
-values (2,'SomsriRakdee(SJ-3)','somsri.rak@mail.kmutt.ac.th','2022-05-27 9:30:00',30,'ขอปรึกษาปัญหาเพื่อนไม่ช่วยงาน',1);
-insert into event (eventId,bookingName,bookingEmail,eventStartTime,eventDuration,eventCategoryId)
-values (3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th','2022-05-23 16:30:00',15,3);
-insert into event (eventId,bookingName,bookingEmail,eventStartTime,eventDuration,eventCategoryId)
-values (4,'หิวข้าวงับ','eiei.za@kmutt.ac.th','2022-05-29 8:30:00',15,1);
-insert into event (eventId,bookingName,bookingEmail,eventStartTime,eventDuration,eventCategoryId)
-values (5,'test','test.test@kmutt.ac.th','2022-05-30 01:30:00',20,2);
 
 create user 'root'@'%' identified by 'rootroot' ;
 grant all privileges on *.* to 'root'@'%' ;
